e261fda4f42cd37da75689ed868c4475aa181de0
meggsmith <meggie@google.com>
2016-04-28 17:17:14 -0400

    Done.

--- js/encoder.js
+++ js/encoder.js
@@ -2,7 +2,8 @@ var encode = function(userString){
   //1. Add code to turn string to lowercase
   userString = userString.toLowerCase();
 
- userString.replace(/a/g, "4");
+  userString = userString.replace(/a/g, "4").replace(/e/g, "3").replace(/i/g, "1").replace(/o/g, "0").replace(/ /g, "$");
+
   
   // 2. Add code to change each instance of a, e, i, o to the appropriate number based on l33n rules
 
